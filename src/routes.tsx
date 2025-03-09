4970a17ed6ee45ffd31528c3f875794f5bfae9d7
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-03-09 17:27:34 -0300

    feat: add dark and light mode

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
@@ -16,12 +17,16 @@ export const router = createBrowserRouter([
     ]
   },
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
     children: [
       {
         path: '/sign-in',
         element: <SignIn />
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />
       }
     ]
   }
